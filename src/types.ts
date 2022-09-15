5c103755d2200b1938eb7272c88639df15ca7b38
Wolfgang Werner <mail@wolfgang-werner.net>
2022-09-15 17:36:28 +0200

    Init

---
+++ src/types.ts
@@ -0,0 +1,64 @@
+export type ADFNodeType =
+	| 'doc'
+	| 'blockquote'
+	| 'bulletList'
+	| 'codeBlock'
+	| 'emoji'
+	| 'hardBreak'
+	| 'heading'
+	| 'inlineCard'
+	| 'listItem'
+	| 'mediaGroup'
+	| 'mediaSingle'
+	| 'mention'
+	| 'orderedList'
+	| 'panel'
+	| 'paragraph'
+	| 'rule'
+	| 'table'
+	| 'tableCell'
+	| 'tableHeader'
+	| 'tableRow'
+	| 'text';
+
+export type ADFMarkType =
+	| 'code'
+	| 'em'
+	| 'link'
+	| 'strike'
+	| 'strong'
+	| 'subsup'
+	| 'textColor'
+	| 'underline';
+
+export interface ADFEntityMark {
+	type: ADFMarkType;
+	attrs?: {
+		[name: string]: string;
+	};
+}
+
+export interface ADFEntity {
+	type: ADFNodeType;
+	attrs?: {
+		[name: string]: unknown;
+	};
+	content?: Array<ADFEntity>;
+	marks?: Array<ADFEntityMark>;
+	text?: string;
+	[key: string]: any;
+}
+
+export type MarkMapper<T> = (mark: ADFEntityMark, next: () => T) => T;
+export type NodeMapper<T> = (node: ADFEntity, processChildren: () => T[]) => T;
+export type Formatter<T> = {
+	default: NodeMapper<T>;
+	nodes: {
+		[nodeType in ADFNodeType]?: NodeMapper<T>;
+	};
+	marks: {
+		[nodeType in ADFNodeType]?: {
+			[markType in ADFMarkType]?: MarkMapper<T>;
+		};
+	};
+};
---
+++ test/parser.test.ts
@@ -0,0 +1,120 @@
+import { formatAdf } from "../";
+import { ADFEntity, Formatter } from "../";
+
+const simpleAdf: ADFEntity = {
+	version: 1,
+	type: 'doc',
+	content: [
+		{
+			type: 'paragraph',
+			content: [
+				{
+					type: 'text',
+					text: 'Hello ',
+				},
+				{
+					type: 'text',
+					text: 'world',
+					marks: [
+						{
+							type: 'strong',
+						},
+					],
+				},
+			],
+		},
+		{
+			type: 'paragraph',
+			content: [
+				{
+					type: 'text',
+					text: 'Hello ',
+				},
+				{
+					type: 'text',
+					text: 'ADF',
+				},
+			],
+		},
+	],
+};
+
+describe(`ADF parser sample`, () => {
+	it('should strip markup', () => {
+		const stripped = formatAdf(simpleAdf, {
+			default: () => '',
+			nodes: {
+				doc: (_n, children) => children().join(''),
+				paragraph: (_n, children) => children().join(''),
+				text: (t) => t.text || '',
+			},
+			marks: {},
+		});
+		expect(stripped).toBe('Hello worldHello ADF');
+	});
+
+	it('should visit every node', () => {
+		const toNestedString = (node: ADFEntity, processChildren: () => string[]) =>
+			`${node.type}(${processChildren().join(',')})`;
+
+		const formatter: Formatter<string> = {
+			default: () => '',
+			nodes: {
+				doc: toNestedString,
+				paragraph: toNestedString,
+				text: (e) => e.type,
+			},
+			marks: {},
+		};
+
+		const result = formatAdf(simpleAdf, formatter);
+		expect(result).toBe('doc(paragraph(text,text),paragraph(text,text))');
+	});
+
+	it("won't skip block nodes without formatter implementations", async () => {
+		const adf: ADFEntity = {
+			type: 'doc',
+			content: [
+				{
+					type: 'paragraph',
+					content: [
+						{
+							type: 'paragraph',
+							content: [
+								{
+									type: 'text',
+									text: 'my data',
+								},
+							],
+						},
+					],
+				},
+			],
+		};
+
+		const formatter: Formatter<string | undefined> = {
+			default: (_e, c) => c().join(''),
+			nodes: {
+				text: (t) => t.text,
+			},
+			marks: {},
+		};
+
+		expect(formatAdf(adf, formatter)).toBe('my data');
+	});
+
+	it('can exclude sub-trees per type', () => {
+		const formatter: Formatter<string> = {
+			default: () => '',
+			nodes: {
+				doc: (_e, children) => children().join(''),
+				paragraph: () => 'p',
+				text: () => 'This is never called, as paragraph does not call children()'
+			},
+			marks: {},
+		};
+
+		const result = formatAdf(simpleAdf, formatter);
+		expect(result).toBe('pp');
+	});
+});
