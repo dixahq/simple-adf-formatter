4ae136997551a8927e93040f25c6187d0dcd7daa
Wolfgang Werner <mail@wolfgang-werner.net>
2022-09-16 09:17:18 +0200

    Make the linter happy

--- src/parser.ts
+++ src/parser.ts
@@ -1,51 +1,52 @@
-import { ADFEntity, Formatter } from "./types";
-
+import { ADFEntity, Formatter } from './types';
 
 /**
  * Formats the given ADF node using the given formatter
  * @param node @param formatter @returns Formatted object of type T
  */
- export const formatAdf = <T>(node: ADFEntity, formatter: Formatter<T>): T => {
-	/*
-	 * Composes the applicable mark format functions for the current node.
-	 *
-	 * @param content T @returns a function to mark up the node's content using
-	 * the formatters mark functions for the node's marks
-	 */
-	const applyMarkup = (content: T) =>
-		(node.marks || [])
-			.map((mark) => ({
-				formatterFunction: formatter.marks[node.type]?.[mark.type],
-				mark,
-			}))
-			.reduce(
-				(prev, curr) => () =>
-					curr.formatterFunction?.(curr.mark, prev) || content,
-				() => content,
-			)();
+export const formatAdf = <T>(node: ADFEntity, formatter: Formatter<T>): T => {
+  /*
+   * Composes the applicable mark format functions for the current node.
+   *
+   * @param content T @returns a function to mark up the node's content using
+   * the formatters mark functions for the node's marks
+   */
+  const applyMarkup = (content: T) =>
+    (node.marks || [])
+      .map(mark => ({
+        formatterFunction: formatter.marks[node.type]?.[mark.type],
+        mark,
+      }))
+      .reduce(
+        (prev, curr) => () =>
+          curr.formatterFunction?.(curr.mark, prev) || content,
+        () => content
+      )();
 
-	/*
-	 * Returns a function recursing through the given node's children tree by
-	 * calling itself for every child node.
-	 *
-	 * @param node the node to process @param formatter @returns a function to
-	 * render the node's children to be used in the formatter (second parameter of
-	 * NodeMapper)
-	 *
-	 * @returns parameter-less function to process children
-	 */
-	const processChildren =
-		(node: ADFEntity, formatter: Formatter<T>) => (): T[] =>
-			node.content // all block nodes have content
-				? node.content.map((child) => formatAdf(child, formatter))
-				: [];
+  /*
+   * Returns a function recursing through the given node's children tree by
+   * calling itself for every child node.
+   *
+   * @param node the node to process @param formatter @returns a function to
+   * render the node's children to be used in the formatter (second parameter of
+   * NodeMapper)
+   *
+   * @returns parameter-less function to process children
+   */
+  const processChildren = (
+    node: ADFEntity,
+    formatter: Formatter<T>
+  ) => (): T[] =>
+    node.content // all block nodes have content
+      ? node.content.map(child => formatAdf(child, formatter))
+      : [];
 
-	/*
-	 * Apply the composed markup function to the curried processChildren function
-	 * and return the result.
-	 */
-	const formatterOrDefault = formatter.nodes[node.type] || formatter.default;
-	return applyMarkup(
-		formatterOrDefault(node, processChildren(node, formatter)),
-	);
+  /*
+   * Apply the composed markup function to the curried processChildren function
+   * and return the result.
+   */
+  const formatterOrDefault = formatter.nodes[node.type] || formatter.default;
+  return applyMarkup(
+    formatterOrDefault(node, processChildren(node, formatter))
+  );
 };
--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,120 +1,121 @@
-import { formatAdf } from "../";
-import { ADFEntity, Formatter } from "../";
+import { formatAdf } from '../';
+import { ADFEntity, Formatter } from '../';
 
 const simpleAdf: ADFEntity = {
-	version: 1,
-	type: 'doc',
-	content: [
-		{
-			type: 'paragraph',
-			content: [
-				{
-					type: 'text',
-					text: 'Hello ',
-				},
-				{
-					type: 'text',
-					text: 'world',
-					marks: [
-						{
-							type: 'strong',
-						},
-					],
-				},
-			],
-		},
-		{
-			type: 'paragraph',
-			content: [
-				{
-					type: 'text',
-					text: 'Hello ',
-				},
-				{
-					type: 'text',
-					text: 'ADF',
-				},
-			],
-		},
-	],
+  version: 1,
+  type: 'doc',
+  content: [
+    {
+      type: 'paragraph',
+      content: [
+        {
+          type: 'text',
+          text: 'Hello ',
+        },
+        {
+          type: 'text',
+          text: 'world',
+          marks: [
+            {
+              type: 'strong',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'paragraph',
+      content: [
+        {
+          type: 'text',
+          text: 'Hello ',
+        },
+        {
+          type: 'text',
+          text: 'ADF',
+        },
+      ],
+    },
+  ],
 };
 
 describe(`ADF parser sample`, () => {
-	it('should strip markup', () => {
-		const stripped = formatAdf(simpleAdf, {
-			default: () => '',
-			nodes: {
-				doc: (_n, children) => children().join(''),
-				paragraph: (_n, children) => children().join(''),
-				text: (t) => t.text || '',
-			},
-			marks: {},
-		});
-		expect(stripped).toBe('Hello worldHello ADF');
-	});
+  it('should strip markup', () => {
+    const stripped = formatAdf(simpleAdf, {
+      default: () => '',
+      nodes: {
+        doc: (_n, children) => children().join(''),
+        paragraph: (_n, children) => children().join(''),
+        text: t => t.text || '',
+      },
+      marks: {},
+    });
+    expect(stripped).toBe('Hello worldHello ADF');
+  });
 
-	it('should visit every node', () => {
-		const toNestedString = (node: ADFEntity, processChildren: () => string[]) =>
-			`${node.type}(${processChildren().join(',')})`;
+  it('should visit every node', () => {
+    const toNestedString = (node: ADFEntity, processChildren: () => string[]) =>
+      `${node.type}(${processChildren().join(',')})`;
 
-		const formatter: Formatter<string> = {
-			default: () => '',
-			nodes: {
-				doc: toNestedString,
-				paragraph: toNestedString,
-				text: (e) => e.type,
-			},
-			marks: {},
-		};
+    const formatter: Formatter<string> = {
+      default: () => '',
+      nodes: {
+        doc: toNestedString,
+        paragraph: toNestedString,
+        text: e => e.type,
+      },
+      marks: {},
+    };
 
-		const result = formatAdf(simpleAdf, formatter);
-		expect(result).toBe('doc(paragraph(text,text),paragraph(text,text))');
-	});
+    const result = formatAdf(simpleAdf, formatter);
+    expect(result).toBe('doc(paragraph(text,text),paragraph(text,text))');
+  });
 
-	it("won't skip block nodes without formatter implementations", async () => {
-		const adf: ADFEntity = {
-			type: 'doc',
-			content: [
-				{
-					type: 'paragraph',
-					content: [
-						{
-							type: 'paragraph',
-							content: [
-								{
-									type: 'text',
-									text: 'my data',
-								},
-							],
-						},
-					],
-				},
-			],
-		};
+  it("won't skip block nodes without formatter implementations", async () => {
+    const adf: ADFEntity = {
+      type: 'doc',
+      content: [
+        {
+          type: 'paragraph',
+          content: [
+            {
+              type: 'paragraph',
+              content: [
+                {
+                  type: 'text',
+                  text: 'my data',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
 
-		const formatter: Formatter<string | undefined> = {
-			default: (_e, c) => c().join(''),
-			nodes: {
-				text: (t) => t.text,
-			},
-			marks: {},
-		};
+    const formatter: Formatter<string | undefined> = {
+      default: (_e, c) => c().join(''),
+      nodes: {
+        text: t => t.text,
+      },
+      marks: {},
+    };
 
-		expect(formatAdf(adf, formatter)).toBe('my data');
-	});
+    expect(formatAdf(adf, formatter)).toBe('my data');
+  });
 
-	it('can exclude sub-trees per type', () => {
-		const formatter: Formatter<string> = {
-			default: () => '',
-			nodes: {
-				doc: (_e, children) => children().join(''),
-				paragraph: () => 'p',
-				text: () => 'This is never called, as paragraph does not call children()'
-			},
-			marks: {},
-		};
+  it('can exclude sub-trees per type', () => {
+    const formatter: Formatter<string> = {
+      default: () => '',
+      nodes: {
+        doc: (_e, children) => children().join(''),
+        paragraph: () => 'p',
+        text: () =>
+          'This is never called, as paragraph does not call children()',
+      },
+      marks: {},
+    };
 
-		const result = formatAdf(simpleAdf, formatter);
-		expect(result).toBe('pp');
-	});
+    const result = formatAdf(simpleAdf, formatter);
+    expect(result).toBe('pp');
+  });
 });
