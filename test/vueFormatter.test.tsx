0510610c2997035663f315cce6de641d35b833c5
Wolfgang Werner <mail@wolfgang-werner.net>
2022-09-18 16:48:55 +0200

    Implement vue tests

--- test/vueFormatter.test.tsx
+++ test/vueFormatter.test.tsx
@@ -21,155 +21,130 @@ describe(`Vue formatting`, () => {
       {
         feature: 'quotes',
         adf: blockquoteAdf,
-        expectedComponent: h('div'),
-
-        // <div>
-        //   <p>A quote follows.</p>
-        //   <blockquote>
-        //     <p>Honk</p>
-        //   </blockquote>
-        //   <p>The end.</p>
-        // </div>
+        expectedComponent: h('div', [
+          h('p', 'A quote follows.'),
+          h('blockquote', h('p', 'Honk')),
+          h('p', 'The end.'),
+        ]),
       },
       {
         feature: 'headings',
         adf: headingsAdf,
-        expectedComponent: h('div'),
-        // <div>
-        //   <h1>Heading 1</h1>
-        //   <p>Text 1</p>
-        //   <h2>Heading 2</h2>
-        //   <p>Text 1.1</p>
-        //   <h3>Heading 3</h3>
-        //   <p>Text 1.1.1</p>
-        //   <h4>Heading 4</h4>
-        //   <p>Text 1.1.1.1</p>
-        //   <p>Text 1.1.1.2</p>
-        //   <h5>Heading 5</h5>
-        //   <p>Text 1.1.1.1.1</p>
-        //   <h6>Heading 6</h6>
-        //   <p>Text 1.1.1.1.1.1</p>
-        // </div>
+        expectedComponent: h('div', [
+          h('h1', 'Heading 1'),
+          h('p', 'Text 1'),
+          h('h2', 'Heading 2'),
+          h('p', 'Text 1.1'),
+          h('h3', 'Heading 3'),
+          h('p', 'Text 1.1.1'),
+          h('h4', 'Heading 4'),
+          h('p', 'Text 1.1.1.1'),
+          h('p', 'Text 1.1.1.2'),
+          h('h5', 'Heading 5'),
+          h('p', 'Text 1.1.1.1.1'),
+          h('h6', 'Heading 6'),
+          h('p', 'Text 1.1.1.1.1.1'),
+        ]),
       },
       {
         feature: 'text markup',
         adf: textMarkupAdf,
-        expectedComponent: h('div'),
-        // <div>
-        //   <p>
-        //     regular
-        //     <br />
-        //     <i>italic</i>
-        //     <br />
-        //     <u>underlined</u>
-        //     <br />
-        //     <span style={{ textDecoration: 'line-through' }}>
-        //       strikethrough
-        //     </span>
-        //     <br />
-        //     <b>bold</b>
-        //     <br />
-        //     sub
-        //     <sub>script</sub>
-        //     <br />
-        //     super
-        //     <sup>script</sup>
-        //     <br />
-        //     <span style={{ textDecoration: 'line-through' }}>
-        //       <b>
-        //         <i>eve</i>
-        //       </b>
-        //     </span>
-        //     <sub>
-        //       <span style={{ textDecoration: 'line-through' }}>
-        //         <b>
-        //           <i>ry</i>
-        //         </b>
-        //       </span>
-        //     </sub>
-        //     <span style={{ textDecoration: 'line-through' }}>
-        //       <b>
-        //         <i>thi</i>
-        //       </b>
-        //     </span>
-        //     <sup>
-        //       <span style={{ textDecoration: 'line-through' }}>
-        //         <b>
-        //           <i>ng</i>
-        //         </b>
-        //       </span>
-        //     </sup>
-        //   </p>
-        // </div>
+        expectedComponent: h('div', [
+          h('p', [
+            'regular',
+            h('br'),
+            h('i', 'italic'),
+            h('br'),
+            h('u', 'underlined'),
+            h('br'),
+            h(
+              'span',
+              { style: { textDecoration: 'line-through' } },
+              'strikethrough'
+            ),
+            h('br'),
+            h('b', 'bold'),
+            h('br'),
+            'sub',
+            h('sub', 'script'),
+            h('br'),
+            'super',
+            h('sup', 'script'),
+            h('br'),
+            h(
+              'span',
+              { style: { textDecoration: 'line-through' } },
+              h('b', [h('i', 'eve')])
+            ),
+            h(
+              'sub',
+              h('span', { style: { textDecoration: 'line-through' } }, [
+                h('b', [h('i', 'ry')]),
+              ])
+            ),
+            h(
+              'span',
+              { style: { textDecoration: 'line-through' } },
+              h('b', [h('i', 'thi')])
+            ),
+            h(
+              'sup',
+              h('span', { style: { textDecoration: 'line-through' } }, [
+                h('b', [h('i', 'ng')]),
+              ])
+            ),
+          ]),
+        ]),
       },
       {
         feature: 'links',
         adf: linkAdf,
-        // Stop prettier from including line breaks and {' '} which changes the resulting JSX
-        //prettier-ignore
-        expectedComponent: h('div'),
-        //  <div>
-        //     <p>Go to <a href="https://dixa.com" target="_blank" rel="noreferrer">Dixa</a>!</p>
-        //   </div> *
+        expectedComponent: h('div', [
+          h('p', [
+            'Go to ',
+            h(
+              'a',
+              { href: 'https://dixa.com', target: '_blank', rel: 'noreferrer' },
+              'Dixa'
+            ),
+            '!',
+          ]),
+        ]),
       },
       {
         feature: 'inline code',
         adf: inlineCodeAdf,
-        expectedComponent: h('div'),
-        // <div>
-        //   <p>
-        //     Hello <code>code</code>.
-        //   </p>
-        // </div>
+        expectedComponent: h('div', h('p', ['Hello ', h('code', 'code'), '.'])),
       },
       {
         feature: 'code block',
         adf: codeBlockAdf,
-        expectedComponent: h('div'),
-        // <div>
-        //   <p>Code follows.</p>
-        //   <pre>
-        //     <code>Hello World</code>
-        //   </pre>
-        // </div>
+        expectedComponent: h('div', [
+          h('p', 'Code follows.'),
+          h('pre', h('code', '10 PRINT "hah hah"\n20 GOTO 10')),
+        ]),
       },
       {
         feature: 'unordered lists',
         adf: bulletListAdf,
-        expectedComponent: h('div'),
-        // <div>
-        //   <ul>
-        //     <li>
-        //       <p>one</p>
-        //     </li>
-        //     <li>
-        //       <p>two</p>
-        //     </li>
-        //     <li>
-        //       <p>nine</p>
-        //     </li>
-        //   </ul>
-        // </div>
+        expectedComponent: h('div', [
+          h('ul', [
+            h('li', h('p', 'one')),
+            h('li', h('p', 'two')),
+            h('li', h('p', 'nine')),
+          ]),
+        ]),
       },
       {
         feature: 'ordered lists',
         adf: orderedListAdf,
         expectedComponent: h('div', [
-          h('ol', [h('li', 'one'), h('li', 'two'), h('li', 'nine')]),
+          h('ol', [
+            h('li', h('p', 'one')),
+            h('li', h('p', 'two')),
+            h('li', h('p', 'nine')),
+          ]),
         ]),
-        // <div>
-        //   <ol>
-        //     <li>
-        //       <p>one</p>
-        //     </li>
-        //     <li>
-        //       <p>two</p>
-        //     </li>
-        //     <li>
-        //       <p>nine</p>
-        //     </li>
-        //   </ol>
-        // </div>
       },
     ];
 
